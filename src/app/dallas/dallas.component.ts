ab48bf658479690401b78663579ea2287c89803b
ariellat16 <ariellat16@gmail.com>
2021-06-18 15:40:25 -0400

    initial commit

---
+++ src/app/dallas/dallas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dallas',
+  templateUrl: './dallas.component.html',
+  styleUrls: ['./dallas.component.scss']
+})
+export class DallasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/jacksonville/jacksonville.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jacksonville',
+  templateUrl: './jacksonville.component.html',
+  styleUrls: ['./jacksonville.component.scss']
+})
+export class JacksonvilleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
