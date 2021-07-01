86ba05044b22f54fd6469ab75f1d023970d4129a
ariellat16 <86077376+ariellat16@users.noreply.github.com>
2021-07-01 17:01:39 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JacksonvilleComponent } from './jacksonville/jacksonville.component';
+import { DallasComponent } from './dallas/dallas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jacksonville', component: JacksonvilleComponent },
+  { path: 'dallas', component: DallasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
